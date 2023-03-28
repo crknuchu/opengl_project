c0d3ba77d3ad66e523ebc851d3121f53ecbfa2cc
crknuchu <mihailo.bogdanovic@gmail.com>
2023-03-28 17:11:03 +0200

    implement texture on rectangle

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#define STB_IMAGE_IMPLEMENTATION
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "include/Shader.h"
+#include "include/stb_image.h"
+#include <filesystem>
+#include <unistd.h>
+
 
 void framebuffer_size_callback(GLFWwindow *window,int width,int height);
 void update(GLFWwindow *window);
@@ -33,10 +38,10 @@ int main() {
     }
 
     float vertices[] = {
-            0.5f,  0.5f, 0.0f,  // top right
-            0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
-            -0.5f,  0.5f, 0.0f   // top left
+            0.5f,  0.5f, 0.0f,  1,1,// top right
+            0.5f, -0.5f, 0.0f,  1,0,// bottom right
+            -0.5f, -0.5f, 0.0f,  0,0,// bottom left
+            -0.5f,  0.5f, 0.0f,   0,1// top left
     };
     unsigned int indices[] = {  // note that we start from 0!
             0, 1, 3,  // first Triangle
@@ -45,7 +50,6 @@ int main() {
 
     Shader shader("/home/matf-racunarska-grafika/Desktop/rg_projekat/resources/shaders/vertexShader.vs",
                   "/home/matf-racunarska-grafika/Desktop/rg_projekat/resources/shaders/fragmentShader.fs");
-
 
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -60,8 +64,42 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1,2,GL_FLOAT,GL_FALSE,5*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //generating texture
+    unsigned int tex0;
+    glGenTextures(1,&tex0);
+    glBindTexture(GL_TEXTURE_2D,tex0);
+
+    //wrap
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT);
+
+    //filter
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+    //load img
+    int width,height,nChannel;
+    unsigned char* data = stbi_load("/home/matf-racunarska-grafika/Desktop/rg_projekat/resources/textures/container.jpg",&width,&height,&nChannel,0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cerr<<"failed to load texture";
+        return -1;
+    }
+
+    stbi_image_free(data);
+
+    shader.use();
+    shader.setUniform1i("t0",0);
+
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -73,7 +111,8 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
-        shader.setUniform4f("gColor",1.0,0.0,0.0,1.0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D,tex0);
 
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT,0);
@@ -84,6 +123,7 @@ int main() {
     glDeleteVertexArrays(1,&VAO);
     glDeleteBuffers(1,&VBO);
     glDeleteBuffers(1,&EBO);
+    shader.deleteProgram();
     glfwTerminate();
 
     return 0;
