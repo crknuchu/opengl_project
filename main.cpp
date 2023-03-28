735e1b0fe5baf06892eeb55dda8b4272d84fc866
crknuchu <mihailo.bogdanovic@gmail.com>
2023-03-28 21:31:24 +0200

    add 10 cubes to scene

--- main.cpp
+++ main.cpp
@@ -85,6 +85,19 @@ int main() {
 //            0, 1, 3,  // first Triangle
 //            1, 2, 3   // second Triangle
 //    };
+    // world space positions of our cubes
+    glm::vec3 cubePositions[] = {
+            glm::vec3( 0.0f,  0.0f,  0.0f),
+            glm::vec3( 2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3( 2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f,  3.0f, -7.5f),
+            glm::vec3( 1.3f, -2.0f, -2.5f),
+            glm::vec3( 1.5f,  2.0f, -2.5f),
+            glm::vec3( 1.5f,  0.2f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 
     glEnable(GL_DEPTH_TEST);
 
@@ -162,7 +175,7 @@ int main() {
         glm::mat4 view          = glm::mat4(1.0f);
         glm::mat4 projection    = glm::mat4(1.0f);
         model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -6.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(shader.getId(), "model");
@@ -177,8 +190,17 @@ int main() {
         glBindTexture(GL_TEXTURE_2D,tex0);
 
         glBindVertexArray(VAO);
-//        glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT,0);
-        glDrawArrays(GL_TRIANGLES,0,36);
+
+        for(int i=0; i<10;i++){
+            glm::mat4 model = glm::mat4(1.0f);
+            float angle = 20.0f * i;
+            model = glm::translate(model,cubePositions[i]);
+            model = glm::rotate(model,glm::radians(angle),glm::vec3(1.0,0.3,0.4));
+            shader.setMat4("model",model);
+            glDrawArrays(GL_TRIANGLES,0,36);
+        }
+
+//        glDrawArrays(GL_TRIANGLES,0,36);
 
         glfwSwapBuffers(window);
     }
