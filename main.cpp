23de5bb0ba31f1f045a35a2d5f1c64280684d730
crknuchu <mihailo.bogdanovic@gmail.com>
2023-03-30 18:26:04 +0200

    implement specular lighting

--- main.cpp
+++ main.cpp
@@ -140,6 +140,7 @@ int main() {
         lightingShader.setVec3("objectColor", glm::vec3 (1.0f, 0.5f, 0.31f));
         lightingShader.setVec3("lightColor",  glm::vec3(1.0f, 1.0f, 1.0f));
         lightingShader.setVec3("lightPos",lightPos);
+        lightingShader.setVec3("viewPos",camera.Position);
 
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)800 / (float)600, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
--- resources/shaders/lightingShader.fs
+++ resources/shaders/lightingShader.fs
@@ -7,6 +7,7 @@ in vec3 FragPos;
 uniform vec3 lightPos;
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+uniform vec3 viewPos;
 
 void main()
 {
@@ -18,7 +19,14 @@ void main()
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
+    //specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(lightDir, norm);
+    float shiness = 32.0;
+    float specularStrength = 0.5;
+    float spec = pow(max(dot(viewDir, reflectDir),0.0),shiness);
+    vec3 specular = spec * lightColor * specularStrength;
 
-    vec3 result = (ambient + diffuse) * objectColor;
+    vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
 }
